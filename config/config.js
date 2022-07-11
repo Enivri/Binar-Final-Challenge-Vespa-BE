f0620c5b455265a1c12b35cf0b081289a24ba2d6
Enivri <Irvineidris24@gmail.com>
2022-07-11 20:22:20 +0700

    commit

--- config/config.js
+++ config/config.js
@@ -22,16 +22,16 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    username: "wuicilrtrjxcte",
+    password: "8dab27229eaba68befb4b810c78c92b309dd0d39f4ce8af4c5f6091e4959b3cb",
+    database: "d95qhsbvcghl4e",
+    host: "ec2-44-198-82-71.compute-1.amazonaws.com",
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
-    },
+    }
   },
 };
